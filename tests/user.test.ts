fd83216e3d9523c8e95272ec91480c14b777b8d8
Mahmoud <itsmahmoud.dev@gmail.com>
2026-05-27 12:09:55 +0300

    test: add proper account activation tests

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -26,14 +26,41 @@ test("Register a duplicate email", () => {
   });
 });
 
-  if (!user) {
-    console.log("no user");
-    return;
-  }
+test("Activate user", async () => {
+  const user = await store.users.repository.findOneBy({ email: testEmail });
 
-  const activated = await store.users.activateUser(user?.activationToken!);
+  expect(user).not.toBeNull();
+  expect(user?.activationToken).not.toBeNull();
+  expect(user?.activationToken).not.toBeUndefined();
 
-  expect(activated).toBe(true);
+  const result = expect(store.users.activateUser(user!.activationToken!));
+
+  result.resolves.toBeTrue();
+});
+
+test("Activate user with an expired token", async () => {
+  const user = await store.users.repository
+    .create({
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      activationToken: "1234",
+      activationTokenExpiry: new Date(Date.now() - 10 * 60 * 1000),
+    })
+    .save();
+
+  expect(user).not.toBeNull();
+  expect(user?.activationToken).not.toBeNull();
+  expect(user?.activationToken).not.toBeUndefined();
+
+  const result = expect(store.users.activateUser(user!.activationToken!));
+
+  result.rejects.toThrow();
+  result.rejects.toMatchObject({
+    code: "U600",
+    message: expect.any(String),
+    cause: expect.any(String),
+  });
 });
 
 test("Log user in", async () => {
