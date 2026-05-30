b00887933bfbf95cf1500fe230bfa6dd0ea7a60c
Mahmoud <itsmahmoud.dev@gmail.com>
2026-05-30 16:07:41 +0300

    feat: error enum type

---
+++ src/types/error.ts
@@ -0,0 +1,10 @@
+export enum UserErrorCodes {
+  TokenInvalidOrExpired = "U600",
+  InvalidEmailOrPassword = "U601",
+  AccountNotVerified = "U602",
+  EmailAlreadyRegistered = "U603",
+}
+
+export enum ProductErrorCodes {
+  BarcodeAlreadyExists = "P600",
+}
