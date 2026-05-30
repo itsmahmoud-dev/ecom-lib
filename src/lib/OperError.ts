380b46686bcd43917ec6965d7b0bbf8548708556
Mahmoud <itsmahmoud.dev@gmail.com>
2026-05-30 16:09:10 +0300

    refactor: OperError can now take the key and value that caused the error

--- src/lib/OperError.ts
+++ src/lib/OperError.ts
@@ -2,17 +2,23 @@ type args = {
   message: string;
   code: string;
   cause?: string;
+  key?: string;
+  value?: string;
 };
 
 export class OperError extends Error {
   code: string;
   override message: string;
   override cause?: string;
+  key?: string;
+  value?: string;
 
-  constructor({ cause, code, message }: args) {
+  constructor({ cause, code, message, key, value }: args) {
     super();
     this.code = code;
     this.message = message;
     this.cause = cause;
+    this.key = key;
+    this.value = value;
   }
 }
