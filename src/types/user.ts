fa76051307a2ca9c684fbb6af5fc42653b7bb38e
Mahmoud <itsmahmoud.dev@gmail.com>
2026-06-18 21:23:33 +0300

    refactor: remove unneeded types

--- src/types/user.ts
+++ src/types/user.ts
@@ -8,17 +8,3 @@ export enum UserStatus {
   PENDING = "pending",
   ACTIVE = "active",
 }
-
-export type CreateUserWithEmailProps = {
-  type: "email";
-  name: string;
-  email: string;
-  password: string;
-};
-
-export type CreateUserWithPhoneNumberProps = {
-  type: "phoneNumber";
-  name: string;
-  phoneNumber: string;
-  password: string;
-};
