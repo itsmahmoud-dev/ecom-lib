59da36871cb709434ed81bb19540c94bd07bb941
Mahmoud <itsmahmoud.dev@gmail.com>
2026-07-18 15:54:03 +0300

    refactor: add default value to cart items schema

--- src/db/schema/cartItems.model.ts
+++ src/db/schema/cartItems.model.ts
@@ -24,7 +24,7 @@ export const cartItems = snakeCase.table(
       .notNull()
       .references(() => productVariants.id, { onDelete: "cascade" }),
 
-    quantity: t.integer(),
+    quantity: t.integer().default(1),
 
     createdAt: t.timestamp({ withTimezone: true }).notNull().defaultNow(),
 
